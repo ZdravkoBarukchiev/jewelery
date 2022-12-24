29e5e6e19752c0e00c544ba38c562bbdedcb3196
ZdravkoBarukchiev <112626604+ZdravkoBarukchiev@users.noreply.github.com>
2022-12-24 14:22:46 +0200

    Update jewelery.js

--- src/components/jewelery.js
+++ src/components/jewelery.js
@@ -1,5 +1,74 @@
 export const Jewellery = () => {
-    return (<div>
+    return (<body class="sub_page">
+        <div className="hero_area">
+            {/* header section strats */}
+            <header className="header_section">
+                <div className="container-fluid">
+                    <nav className="navbar navbar-expand-lg custom_nav-container ">
+                        <a className="navbar-brand" href="index.html">
+                            <img src="images/logo.png" alt="" />
+                            <span>Lodge</span>
+                        </a>
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon" />
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <div className="d-flex ml-auto flex-column flex-lg-row align-items-center">
+                                <ul className="navbar-nav  ">
+                                    <li className="nav-item active">
+                                        <a className="nav-link" href="index.html">
+                                            Home <span className="sr-only">(current)</span>
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="about.html">
+                                            {" "}
+                                            About
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="jewellery.html">
+                                            Jewellery{" "}
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="contact.html">
+                                            Contact us
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        {/*} <a className="nav-link" href="#">
+                                            Login
+                                        </a>*/}
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="quote_btn-container ">
+                                {/*<a href="">
+                                    <img src="images/cart.png" alt="" />
+                                    <div className="cart_number">0</div>
+                                </a>*/}
+                                <form className="form-inline">
+                                    <button
+                                        className="btn  my-2 my-sm-0 nav_search-btn"
+                                        type="submit"
+                                    />
+                                </form>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            {/* end header section */}
+        </div>
 
         {/* item section */}
         <div className="item_section layout_padding2">
@@ -115,7 +184,7 @@ export const Jewellery = () => {
                         </div>
                         <div className="col-md-3">
                             <div className="info_contact">
-                               {/* <a href="">
+                                {/* <a href="">
                                     <img src="images/location.png" alt="" />
                                     <span>Address</span>
                                 </a>*/}
@@ -157,7 +226,7 @@ export const Jewellery = () => {
                             <h5 className="info_heading">Follow Us</h5>
                         </div>
                         <div className="social_box">
-                           {/*} <a href="">
+                            {/*} <a href="">
                                 <img src="images/fb.png" alt="" />
                             </a>
                             <a href="">
@@ -184,7 +253,7 @@ export const Jewellery = () => {
         </section>
         {/* footer section */}
 
-    </div>)
+    </body>)
 
 
 }
