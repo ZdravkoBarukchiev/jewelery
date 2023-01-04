a499fa7190699d57a9741d162218ccb74656fc2c
ZdravkoBarukchiev <112626604+ZdravkoBarukchiev@users.noreply.github.com>
2023-01-04 09:53:45 +0200

    Create Logout.js

---
+++ src/components/logout/Logout.js
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginContext } from "../../context/loginContext";
+import { useEffect } from "react";
+
+export const Logout = () => {
+    const navigate = useNavigate();
+    const { loginData, userLogout } = useContext(LoginContext);
+    const url = "http://localhost:3030/users/logout";
+    useEffect(() => {
+        fetch(url, {
+            headers: {
+                "X-Authorization": loginData.accessToken,
+            },
+        });
+        userLogout();
+        navigate("/");
+    }, [userLogout, navigate, loginData.accessToken]);
+    return null;
+};
