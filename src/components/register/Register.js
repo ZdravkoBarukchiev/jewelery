304ee661a8abd4c996561f56a7bf2650b07e79fb
ZdravkoBarukchiev <112626604+ZdravkoBarukchiev@users.noreply.github.com>
2023-01-06 10:55:43 +0200

    updated

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,13 +1,11 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { LoginContext } from "../../context/loginContext";
+import { HeaderNavigation } from "../headerNavigation/HeaderNavigation";
+
 export const Register = () => {
     const navigate = useNavigate();
     const { userLogin } = useContext(LoginContext);
-    const search = (e) => {
-        e.preventDefault()
-        console.log('search');
-    }
     const onSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
@@ -32,76 +30,7 @@ export const Register = () => {
     return (
         <div className="hero_area">
             {/* header section */}
-            <header className="header_section">
-                <div className="container-fluid">
-                    <nav className="navbar navbar-expand-lg custom_nav-container ">
-                        <Link className="navbar-brand" to="/">
-                            <img src="images/logo.png" alt="" />
-                            <span>Lodge</span>
-                        </Link>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon" />
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <div className="d-flex ml-auto flex-column flex-lg-row align-items-center">
-                                <ul className="navbar-nav  ">
-                                    <li className="nav-item active">
-                                        <Link className="nav-link" to="/">
-                                            Home <span className="sr-only">(current)</span>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/about">
-                                            {" "}
-                                            About
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/jewellery">
-                                            Jewellery{" "}
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/contacts">
-                                            Contact us
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/login">
-                                            Login
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/register">
-                                            Register
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="quote_btn-container ">
-                                <Link to="/">
-                                    <img src="images/cart.png" alt="" />
-                                    <div className="cart_number">0</div>
-                                </Link>
-                                <form className="form-inline" onSubmit={search}>
-                                    <button
-                                        className="btn  my-2 my-sm-0 nav_search-btn"
-                                        type="submit"
-                                    />
-                                </form>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </header>
+            <HeaderNavigation />
             {/* end header section */}
             {/* register section */}
             <section className="contact_section layout_padding">
